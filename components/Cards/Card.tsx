7fc2212c7961b4c60c0f6d4b7ec5128173543a9c
Joel de Leon <joel.dl08@gmail.com>
2022-02-14 02:49:31 -0600

    Fix styles with classnames pkg

--- components/Cards/Card.tsx
+++ components/Cards/Card.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 
 interface CardProps {
@@ -12,26 +13,23 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({
   visible = false,
   amount,
-  onSelection,
   selectedAmount,
   disabled,
   answered,
+  onSelection,
 }) => {
   return (
     <div
-      className={`card ${visible ? "visible" : ""} ${
-        selectedAmount === amount && selectedAmount !== undefined
-          ? "bg-blue-500 text-white border-blue-500"
-          : "bg-transparent border-slate-400"
-      } ${
-        disabled
-          ? "bg-slate-700 text-slate-400 cursor-not-allowed border-slate-700"
-          : "bg-transparent text-slate-400"
-      } ${
-        answered
-          ? "bg-blue-500 text-white"
-          : "bg-transparent border-transparent"
-      } h-20 w-14 m-4 rounded flex justify-center items-center border-2 cursor-pointer`}
+      className={classNames(
+        "h-20 w-14 m-4 rounded flex justify-center items-center border-2 cursor-pointer text-white",
+        {
+          "bg-blue-500 text-white border-blue-500":
+            selectedAmount === amount && selectedAmount !== undefined,
+          "bg-slate-700 text-slate-400 cursor-not-allowed border-slate-700":
+            disabled,
+          "bg-blue-500 text-white": answered,
+        }
+      )}
       onClick={onSelection && !disabled ? () => onSelection(amount) : null}
     >
       <div className="card-content">{visible ? amount : null}</div>
